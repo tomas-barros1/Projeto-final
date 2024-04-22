2b2d49c1ff530b8db8a662995ed99d3eb0a6f900
tomas-barros1 <tomasabbarros3@gmail.com>
2024-04-22 18:38:16 -0300

    correção das validações e importação do sdk do firebase

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,12 +1,16 @@
 let enviarBotao = document.getElementById("enviar");
+let nome;
+let email;
+let confirmarSenha;
+let cnpj;
 
 enviarBotao.addEventListener("click", () => {
-  let nome = document.getElementById("nome").value;
-  let email = document.getElementById("email").value;
-  let senha = document.getElementById("senha").value;
-  let confirmarSenha = document.getElementById("confirmarSenha").value;
-  let cpf = document.getElementById("cpf").value;
-  let cnpj = document.getElementById("cnpj").value;
+  nome = document.getElementById("nome").value;
+  email = document.getElementById("email").value;
+  senha = document.getElementById("senha").value;
+  confirmarSenha = document.getElementById("confirmarSenha").value;
+  cpf = document.getElementById("cpf").value;
+  cnpj = document.getElementById("cnpj").value;
 
   verificarSeEstaVazio();
 
@@ -30,12 +34,59 @@ function verificarSeEstaVazio() {
     email.trim() === "" ||
     senha.trim() === "" ||
     confirmarSenha.trim() === "" ||
-    cpf.trim() ||
-    cnpj.trim()
+    cpf.trim() === "" ||
+    cnpj.trim() === ""
   ) {
     alert("Por favor, preencha todos os campos.");
     return;
   }
+}
+
+function validarCNPJ(cnpj) {
+  cnpj = cnpj.replace(/[^\d]+/g, "");
+
+  if (cnpj == "") return false;
+
+  if (cnpj.length != 14) return false;
+
+  if (
+    cnpj == "00000000000000" ||
+    cnpj == "11111111111111" ||
+    cnpj == "22222222222222" ||
+    cnpj == "33333333333333" ||
+    cnpj == "44444444444444" ||
+    cnpj == "55555555555555" ||
+    cnpj == "66666666666666" ||
+    cnpj == "77777777777777" ||
+    cnpj == "88888888888888" ||
+    cnpj == "99999999999999"
+  )
+    return false;
+
+  tamanho = cnpj.length - 2;
+  numeros = cnpj.substring(0, tamanho);
+  digitos = cnpj.substring(tamanho);
+  soma = 0;
+  pos = tamanho - 7;
+  for (i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2) pos = 9;
+  }
+  resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+  if (resultado != digitos.charAt(0)) return false;
+
+  tamanho = tamanho + 1;
+  numeros = cnpj.substring(0, tamanho);
+  soma = 0;
+  pos = tamanho - 7;
+  for (i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2) pos = 9;
+  }
+  resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+  if (resultado != digitos.charAt(1)) return false;
+
+  return true;
 }
 
 function validarCampos() {
@@ -43,12 +94,13 @@ function validarCampos() {
     alert("As senhas não coincidem");
   } else if (!validarEmail(email)) {
     alert("E-mail inválido");
+  } else if (!validarCNPJ(cnpj)) {
+    alert("CNPJ inválido");
   } else {
     let pessoa = {
       nome: nome,
       email: email,
       senha: senha,
-      confirmarSenha: confirmarSenha,
     };
   }
 }
