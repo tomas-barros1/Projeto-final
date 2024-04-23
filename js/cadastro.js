3be1d82908ef9193d6cc193654709802ea8e5c86
tomas-barros1 <tomasabbarros3@gmail.com>
2024-04-23 20:52:05 -0300

    criação de usuário no firabase auth funcionando

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,7 +1,17 @@
+import { app } from "./firebase-sdk.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+
+const auth = getAuth(app);
+
 let enviarBotao = document.getElementById("enviar");
 let nome;
 let email;
+let senha;
 let confirmarSenha;
+let cpf;
 let cnpj;
 
 enviarBotao.addEventListener("click", () => {
@@ -12,9 +22,20 @@ enviarBotao.addEventListener("click", () => {
   cpf = document.getElementById("cpf").value;
   cnpj = document.getElementById("cnpj").value;
 
-  verificarSeEstaVazio();
-
-  validarCampos();
+  if ((!verificarSeEstaVazio()) && (validarCampos())) {
+    createUserWithEmailAndPassword(auth, email, senha)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log("Usuário criado com sucesso:", user);
+        alert("Usuário criado com sucesso!");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Erro ao criar usuário:", errorMessage);
+        alert("Erro ao criar usuário!");
+      });
+  }
 });
 
 function validarEmail(email) {
@@ -38,53 +59,24 @@ function verificarSeEstaVazio() {
     cnpj.trim() === ""
   ) {
     alert("Por favor, preencha todos os campos.");
-    return;
+    return true;
   }
+  return false;
 }
 
 function validarCNPJ(cnpj) {
-  cnpj = cnpj.replace(/[^\d]+/g, "");
+  var b = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+  var c = String(cnpj).replace(/[^\d]/g, "");
 
-  if (cnpj == "") return false;
+  if (c.length !== 14) return false;
 
-  if (cnpj.length != 14) return false;
+  if (/0{14}/.test(c)) return false;
 
-  if (
-    cnpj == "00000000000000" ||
-    cnpj == "11111111111111" ||
-    cnpj == "22222222222222" ||
-    cnpj == "33333333333333" ||
-    cnpj == "44444444444444" ||
-    cnpj == "55555555555555" ||
-    cnpj == "66666666666666" ||
-    cnpj == "77777777777777" ||
-    cnpj == "88888888888888" ||
-    cnpj == "99999999999999"
-  )
-    return false;
+  for (var i = 0, n = 0; i < 12; n += c[i] * b[++i]);
+  if (c[12] != ((n %= 11) < 2 ? 0 : 11 - n)) return false;
 
-  tamanho = cnpj.length - 2;
-  numeros = cnpj.substring(0, tamanho);
-  digitos = cnpj.substring(tamanho);
-  soma = 0;
-  pos = tamanho - 7;
-  for (i = tamanho; i >= 1; i--) {
-    soma += numeros.charAt(tamanho - i) * pos--;
-    if (pos < 2) pos = 9;
-  }
-  resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-  if (resultado != digitos.charAt(0)) return false;
-
-  tamanho = tamanho + 1;
-  numeros = cnpj.substring(0, tamanho);
-  soma = 0;
-  pos = tamanho - 7;
-  for (i = tamanho; i >= 1; i--) {
-    soma += numeros.charAt(tamanho - i) * pos--;
-    if (pos < 2) pos = 9;
-  }
-  resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-  if (resultado != digitos.charAt(1)) return false;
+  for (var i = 0, n = 0; i <= 12; n += c[i] * b[i++]);
+  if (c[13] != ((n %= 11) < 2 ? 0 : 11 - n)) return false;
 
   return true;
 }
@@ -96,11 +88,14 @@ function validarCampos() {
     alert("E-mail inválido");
   } else if (!validarCNPJ(cnpj)) {
     alert("CNPJ inválido");
+  } else if (!validarFormatoCPF(cpf)) {
+    alert("CPF inválido");
   } else {
     let pessoa = {
       nome: nome,
       email: email,
       senha: senha,
     };
+    return true;
   }
 }
