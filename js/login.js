06e99837fe5589b151eda2440ede6f5319f36894
tomas-barros1 <tomasabbarros3@gmail.com>
2024-04-26 14:37:57 -0300

    remoção de reduncâncias e melhoras no código do login

--- js/login.js
+++ js/login.js
@@ -1,28 +1,36 @@
 import { obterDocumentos } from "./firebase-sdk.js";
 
-let enviar = document.getElementById("enviar");
+const enviar = document.getElementById("enviar");
 
 enviar.addEventListener("click", async () => {
-  let login = document.getElementById("login").value;
-  let senha = document.getElementById("senha").value;
+  const login = document.getElementById("login").value;
+  const senha = document.getElementById("senha").value;
 
   try {
-    const querySnapshot = await obterDocumentos()
-    if (!querySnapshot.empty) {
-      querySnapshot.forEach((doc) => {
-        if (doc.exists) { // Verificar se o documento existe
-          if (doc.data().login === login && doc.data().senha === senha) {
-            alert('Login bem-sucedido!');
-            window.location.href = "html/dashboard.html";
-          } else {
-            alert("Usuário ou senha incorretos!");
-          }
-        } else {
-          alert("Documento não encontrado!");
-        }
-      });
-    } else {
+    const querySnapshot = await obterDocumentos();
+
+    if (querySnapshot.empty) {
       alert("Não há usuários cadastrados!");
+      return;
+    }
+
+    let usuarioEncontrado = false;
+
+    querySnapshot.forEach((doc) => {
+      if (
+        doc.exists &&
+        doc.data().login === login &&
+        doc.data().senha === senha
+      ) {
+        usuarioEncontrado = true;
+      }
+    });
+
+    if (usuarioEncontrado) {
+      alert("Login bem-sucedido!");
+      window.location.href = "html/dashboard.html";
+    } else {
+      alert("Usuário ou senha incorretos!");
     }
   } catch (error) {
     console.error("Erro ao verificar usuário:", error);
