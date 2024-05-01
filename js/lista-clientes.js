70148fe53dd8a0d9d1b88763a9e0dcf232c6593a
henrique27k <joao.h.cerqueira@ba.estudante.senai.br>
2024-05-01 18:48:43 -0300

    lista de clientes

--- js/lista-clientes.js
+++ js/lista-clientes.js
@@ -1,21 +1,20 @@
 async function fetchClientes() {
   try {
-    const response = await fetch("http://localhost:3000/clientes"); // Requisição GET para /clientes
-    const data = await response.json(); // Converte a resposta para JSON
+    const response = await fetch("https://api-ityw.onrender.com/clientes");
+    const data = await response.json(); 
     const clientesLista = document.getElementById("clientes-lista");
 
-    // Limpa a lista de clientes antes de adicionar os novos
     clientesLista.innerHTML = "";
 
-    // Adiciona cada cliente à lista
     data.forEach((cliente) => {
-      const li = document.createElement("li");
-      li.textContent = `Nome: ${cliente.nome}, CPF/CNPJ: ${
-        cliente.cpf || cliente.cnpj
-      }, Email: ${cliente.email}, Endereço: ${cliente.endereco}, Cidade: ${
-        cliente.cidade
-      }`;
-      clientesLista.appendChild(li);
+      const tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td><input type="checkbox"></td>
+        <td>${cliente.nomeFantasia}</td>
+        <td>${cliente.cnpj}</td>
+        <td>${cliente.endereco}</td>
+      `;
+      clientesLista.appendChild(tr);
     });
   } catch (error) {
     console.error("Erro ao buscar clientes:", error);
