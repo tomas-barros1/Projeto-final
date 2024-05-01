cf2b958a7acd77757ab2526da946b330b1210264
Roque Maia <roquemaiarochaneto@gmail.com>
2024-05-01 18:42:20 -0300

    melhorias na tela de login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 import { obterDocumentos } from "./firebase-sdk.js";
 
 const enviar = document.getElementById("enviar");
+const mensagemFlutuante = document.getElementById("mensagem-flutuante");
 
 enviar.addEventListener("click", async () => {
   const login = document.getElementById("login").value;
@@ -10,7 +11,7 @@ enviar.addEventListener("click", async () => {
     const querySnapshot = await obterDocumentos();
 
     if (querySnapshot.empty) {
-      alert("Não há usuários cadastrados!");
+      exibirMensagem("Não há usuários cadastrados!");
       return;
     }
 
@@ -27,12 +28,21 @@ enviar.addEventListener("click", async () => {
     });
 
     if (usuarioEncontrado) {
-      alert("Login bem-sucedido!");
+      exibirMensagem("Login bem-sucedido!");
       window.location.href = "html/dashboard.html";
     } else {
-      alert("Usuário ou senha incorretos!");
+      exibirMensagem("Usuário ou senha incorretos!");
     }
   } catch (error) {
     console.error("Erro ao verificar usuário:", error);
   }
 });
+
+function exibirMensagem(mensagem) {
+  mensagemFlutuante.textContent = mensagem;
+  mensagemFlutuante.style.display = "block";
+  setTimeout(() => {
+    mensagemFlutuante.style.display = "none";
+  }, 5000);
+}
+
